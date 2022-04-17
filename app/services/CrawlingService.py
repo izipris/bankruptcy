eadd5470d17e4dd4eb4a51085336549b8c732212
Idan Zipris <idzipris@ebay.com>
2022-04-18 01:33:39 +0300

    configured heroku chrome driver

--- app/services/CrawlingService.py
+++ app/services/CrawlingService.py
@@ -1,14 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from ..model import Bankrupt
+import os
 
 PERSON_TEXT = 'חייב יחיד'
 NO_INFO = 'אין מידע'
 
 
+def get_chrome_driver():
+    # Local
+    # return webdriver.Chrome(
+    #     executable_path="/Users/idzipris/workspace/Projects/Java/BankruptcyCrawler/chromedriver/chromedriver 4")
+
+    # Heroku
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    return webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+
+
 def get_bankrupts(ids):
-    browser = webdriver.Chrome(
-        executable_path="/Users/idzipris/workspace/Projects/Java/BankruptcyCrawler/chromedriver/chromedriver 4")
+    browser = get_chrome_driver()
     bankrupts = []
     for id in ids:
         bankrupt = Bankrupt.Bankrupt(id)
@@ -41,14 +55,19 @@ def construct_person(browser, person):
     person.set_cancel_reason(browser.find_element_by_id('lstTikGeneralDetails_txtCancelReason').get_attribute('value'))
     person.set_case_status(browser.find_element_by_id('lstTikGeneralDetails_txtTikStatus').get_attribute('value'))
     person.set_authority(browser.find_element_by_id('lstTikGeneralDetails_txtRashutMetapelet').get_attribute('value'))
-    person.set_cancel_date(browser.find_element_by_id('lstTikGeneralDetails_txtTzavCancellationDate').get_attribute('value'))
-    person.set_bankruptcy_date( browser.find_element_by_id('lstTikGeneralDetails_txtTzavPshitaDate').get_attribute('value'))
-    person.set_concentration_date(browser.find_element_by_id('lstTikGeneralDetails_txtTzavKinusDate').get_attribute('value'))
+    person.set_cancel_date(
+        browser.find_element_by_id('lstTikGeneralDetails_txtTzavCancellationDate').get_attribute('value'))
+    person.set_bankruptcy_date(
+        browser.find_element_by_id('lstTikGeneralDetails_txtTzavPshitaDate').get_attribute('value'))
+    person.set_concentration_date(
+        browser.find_element_by_id('lstTikGeneralDetails_txtTzavKinusDate').get_attribute('value'))
 
 
 def construct_company(browser, company):
     company.set_case_id(browser.find_element_by_id('bnrTikTopInfo_grdInfo_lblTikNoKanar_0').text)
     company.set_case_status(browser.find_element_by_id('lstTikGeneralDetails_txtTikStatus').get_attribute('value'))
     company.set_authority(browser.find_element_by_id('lstTikGeneralDetails_txtRashutMetapelet').get_attribute('value'))
-    company.set_destruction_date(browser.find_element_by_id('lstTikGeneralDetails_txtTzavPshitaDate').get_attribute('value'))
-    company.set_delay_date(browser.find_element_by_id('lstTikGeneralDetails_txtTzavCancellationDate').get_attribute('value'))
+    company.set_destruction_date(
+        browser.find_element_by_id('lstTikGeneralDetails_txtTzavPshitaDate').get_attribute('value'))
+    company.set_delay_date(
+        browser.find_element_by_id('lstTikGeneralDetails_txtTzavCancellationDate').get_attribute('value'))
